1fab46d4e5f89894e945923dd6baa93bbcb6fe9d
Mahmoud Said <mahmoudsaid1704@gmail.com>
2025-09-06 01:50:50 +0300

    Add reset-password endpoint

--- qent/urls.py
+++ qent/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 
     path('Downlaod', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('schema/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
 
 ]
