849f6f8470bce40dc576c2d3c194786d0c36286f
Mahmoud Said <mahmoudsaid1704@gmail.com>
2025-09-11 19:55:56 +0300

    Add password reset

--- qent/settings.py
+++ qent/settings.py
@@ -155,16 +155,13 @@ REST_USE_JWT = True
 # ----------------------
 # Email (SMTP)
 # ----------------------
-EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', "django.core.mail.backends.smtp.EmailBackend")
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-
-PASSWORD_RESET_CONFIRM_URL = "password/reset/confirm/{uid}/{token}/"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # ----------------------
 # Static files
