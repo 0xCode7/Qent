ecdc107fdcb369a74cbdad0679b1be879b41ec64
0xCode7 <mahmoudsaid1704@gmail.com>
2026-02-21 15:55:10 +0200

    Handle non_field_error

--- qent/exceptions.py
+++ qent/exceptions.py
@@ -8,26 +8,40 @@ def custom_exception_handler(exc, context):
     if response is None:
         return response
 
-    # Validation Errors (serializer errors)
     if isinstance(exc, ValidationError):
+        errors = {}
+        non_field_messages = []
+
+        for field, msgs in response.data.items():
+            if not isinstance(msgs, list):
+                msgs = [msgs]
+
+            # Global
+            if field in ["non_field_errors", "login_error"]:
+                non_field_messages.extend(msgs)
+            else:
+                errors[field] = msgs
+
+        if non_field_messages:
+            errors["message"] = (
+                non_field_messages[0]
+                if len(non_field_messages) == 1
+                else non_field_messages
+            )
+
         response.data = {
             "message": "Invalid Request",
-            "errors": response.data
+            "errors": errors
         }
+
         return response
 
-    # (Authentication, Permission, NotFound, etc.)
+    # Other errors
     detail = response.data.get("detail")
 
-    if detail:
-        response.data = {
-            "message": detail,
-            "errors": {}
-        }
-    else:
-        response.data = {
-            "message": "Something went wrong",
-            "errors": {}
-        }
+    response.data = {
+        "message": detail if detail else "Something went wrong",
+        "errors": {}
+    }
 
     return response
